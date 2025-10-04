cf384626277b56b3d8631c40a8d01c3e9784f56d
mini667 <mrerbol08@gmail.com>
2025-10-04 15:01:47 +0600

    adding main menu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneButton : MonoBehaviour
+{
+    [SerializeField] private string sceneName; // Название сцены
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(LoadScene);
+        }
+    }
+
+    private void LoadScene()
+    {
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogWarning("Не указано название сцены для кнопки: " + gameObject.name);
+        }
+    }
+}
